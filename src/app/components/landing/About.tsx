e59b1069b814d4eaacdb0e67c8c5d223ac13e0f4
NotJerwee <jerrylin2488@gmail.com>
2024-08-21 16:33:34 -0400

    Create nav to build

--- src/app/components/landing/About.tsx
+++ src/app/components/landing/About.tsx
@@ -1,15 +1,15 @@
 'use client';
 
-import { FC } from 'react';
+import Link from 'next/link';
 
-const About: FC = () => {
+const About = () => {
 	return (
 		<main className="flex flex-col items-center text-center p-30 mt-60">
 		<h1 className="text-5xl font-bold mb-4">SmartSpecs</h1>
 		<p className="text-xl mb-8">Build Your PC</p>
-		<button className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition">
+		<Link href="/build" className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition">
 			Start Building
-		</button>
+		</Link>
 		</main>
 	);
 };
