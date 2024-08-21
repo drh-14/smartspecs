9c2b8d08ac753a53dfba2a7e4a5e4a377900898a
NotJerwee <jerrylin2488@gmail.com>
2024-08-20 23:56:08 -0400

    Add starting button

--- src/app/pages/Landing.tsx
+++ src/app/pages/Landing.tsx
@@ -5,8 +5,13 @@ import { FC } from 'react';
 const Landing: FC = () => {
 	return (
 		<main className="flex flex-col items-center text-center p-30 mt-60">
-			<h1 className="text-5xl font-bold mb-4">SmartSpecs</h1>
-			<p className="text-xl">Build Your PC</p>
+		<h1 className="text-5xl font-bold mb-4">SmartSpecs</h1>
+		<p className="text-xl mb-8">Build Your PC</p>
+		<button
+			className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition"
+		>
+			Get Started
+		</button>
 		</main>
 	);
 };
