4ea762238f1c785ea0816b63f9ce8da6e6e3855b
NotJerwee <jerrylin2488@gmail.com>
2024-08-25 10:55:35 -0400

    Increase navbar size

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -7,35 +7,35 @@ const Navbar: FC = () => {
 	const router = useRouter();
 
 	return (
-		<nav className="bg-gray-800 text-white p-4">
-		<ul className="flex justify-between items-center">
-			<li>
-			<a
-				onClick={() => router.push('/')}
-				className="hover:underline cursor-pointer"
-			>
-				SmartSpecs
-			</a>
-			</li>
-			<div className="flex space-x-4">
-			<li>
-				<a
-				onClick={() => router.push('/login')}
-				className="hover:underline cursor-pointer"
-				>
-				Login
-				</a>
-			</li>
-			<li>
-				<a
-				onClick={() => router.push('/register')}
-				className="hover:underline cursor-pointer"
-				>
-				Register
-				</a>
-			</li>
-			</div>
-		</ul>
+		<nav className="bg-gray-800 text-white p-6 text-lg">
+			<ul className="flex justify-between items-center">
+				<li>
+					<a
+						onClick={() => router.push('/')}
+						className="hover:underline cursor-pointer"
+					>
+						SmartSpecs
+					</a>
+				</li>
+				<div className="flex space-x-6">
+					<li>
+						<a
+							onClick={() => router.push('/login')}
+							className="hover:underline cursor-pointer"
+						>
+							Login
+						</a>
+					</li>
+					<li>
+						<a
+							onClick={() => router.push('/register')}
+							className="hover:underline cursor-pointer"
+						>
+							Register
+						</a>
+					</li>
+				</div>
+			</ul>
 		</nav>
 	);
 };
